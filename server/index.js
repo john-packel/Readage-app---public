22948f2aec7428c0d660ca60f5321da722e9e1ae
John Packel <john.packel@gmail.com>
2017-02-03 19:23:12 -0500

    Not getting dotenv to work so commenting out for now...

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ var app = express();
 var request = require('request');
 var parser = require('xml2json');
 
-require('dotenv').config();
+// var dotenv = require('dotenv').config();
 
 // Ex in documentation: https://devcenter.heroku.com/articles/config-vars
 // const aws = require('aws-sdk');
