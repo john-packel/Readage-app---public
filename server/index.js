d6e0cc4e567b2073b23608152a0d037f23e96202
John Packel <john.packel@gmail.com>
2017-02-04 00:10:29 -0500

    heroku not loading site. Error in || process.env so removed that for now.

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ var app = express();
 var request = require('request');
 var parser = require('xml2json');
 
-require('../.env');
+// require('../.env');
 // require('.env');
 // var buf = new Buffer('BASIC=basic');
 // var config = dotenv.parse(buf)
@@ -20,9 +20,10 @@ require('../.env');
 //   secretAccessKey: process.env.S3_SECRET
 // });
 
-
-var Goodreads_Key = process.env.GOODREADS_KEY || config.GOODREADS_KEY;
-var NYTimes_Article_Key = process.env.NYTIMES_ARTICLE_KEY || config.NYTIMES_ARTICLE_KEY;
+var Goodreads_Key = process.env.GOODREADS_KEY;
+var NYTimes_Article_Key = process.env.NYTIMES_ARTICLE_KEY;
+// var Goodreads_Key = process.env.GOODREADS_KEY || config.GOODREADS_KEY;
+// var NYTimes_Article_Key = process.env.NYTIMES_ARTICLE_KEY || config.NYTIMES_ARTICLE_KEY;
 
 app.use(express.static(__dirname + '/../client'));
 app.use(bodyParser());
